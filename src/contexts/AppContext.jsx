312cc5eba5a063b9e1c9caa416e112be2e3d14b3
Makhan Diakho <makhandiakho95@gmail.com>
2024-03-20 17:52:10 +0000

    new1

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState,useContext } from "react";
 export const AppContext = createContext();
 
 export function AppContextProvider({ children }) {
-  const [preset, setPreset] = useState("roomToBottom");
+  const [preset, setPreset] = useState("fall");
   const [enterAnimation, setEnterAnimation] = useState("");
   const [exitAnimation, setExitAnimation] = useState("");
 
